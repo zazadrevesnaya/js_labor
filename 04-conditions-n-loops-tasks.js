4969a9d0c15553df790e784f4dbb7318ab4c16fd
zazadrevesnaya <petroosetro0@gmail.com>
2025-10-08 10:31:51 +0300

    Update 04-conditions-n-loops-tasks.js

--- 04-conditions-n-loops-tasks.js
+++ 04-conditions-n-loops-tasks.js
@@ -946,15 +946,12 @@ function getFizzBuzz(num) {
    */
   function evaluateTicTacToePosition(position) {
     const lines = [
-      // rows
       [position[0][0], position[0][1], position[0][2]],
       [position[1][0], position[1][1], position[1][2]],
       [position[2][0], position[2][1], position[2][2]],
-      // columns
       [position[0][0], position[1][0], position[2][0]],
       [position[0][1], position[1][1], position[2][1]],
       [position[0][2], position[1][2], position[2][2]],
-      // diagonals
       [position[0][0], position[1][1], position[2][2]],
       [position[0][2], position[1][1], position[2][0]],
     ];
@@ -985,4 +982,5 @@ function getFizzBuzz(num) {
     getCommonDirectoryPath,
     getMatrixProduct,
     evaluateTicTacToePosition,
+
   };
